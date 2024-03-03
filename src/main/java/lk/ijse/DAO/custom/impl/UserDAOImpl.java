c3f1766a3a27e52262087bc2e238c6a8c17cba1c
PiuminiRash <piuminirashmika@gmail.com>
2024-03-03 23:06:00 +0530

    User Saving

--- src/main/java/lk/ijse/DAO/custom/impl/UserDAOImpl.java
+++ src/main/java/lk/ijse/DAO/custom/impl/UserDAOImpl.java
@@ -1,4 +1,49 @@
 package lk.ijse.DAO.custom.impl;
 
-public class UserDAOImpl {
+import lk.ijse.Config.FactoryConfiguration;
+import lk.ijse.DAO.custom.UserDAO;
+import lk.ijse.Entity.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class UserDAOImpl implements UserDAO {
+    Session session = FactoryConfiguration.getInstance().getSession();
+
+    @Override
+    public boolean save(User dto) {
+        try {
+            Transaction transaction = session.beginTransaction();
+            Serializable save = (Serializable) session.save(dto);
+            transaction.commit();
+            return save != null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public boolean update(User dto) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return false;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+
+    @Override
+    public User getItem(String id) {
+        return null;
+    }
 }
