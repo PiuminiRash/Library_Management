840f1c7496cf6e1b09a6805e918822835a25eeec
PiuminiRash <piuminirashmika@gmail.com>
2024-03-03 18:22:08 +0530

    Design LoginForm

--- src/main/java/lk/ijse/Controller/LoginFormController.java
+++ src/main/java/lk/ijse/Controller/LoginFormController.java
@@ -1,4 +1,35 @@
 package lk.ijse.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class LoginFormController {
+    @FXML
+    private TextField txtMail;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    void btnSingInOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSingUpOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void imgHide(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imgView(MouseEvent event) {
+
+    }
 }
