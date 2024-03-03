2d1531600a6f12d1f1a0e8cbe56718a8d3a0b87b
PiuminiRash <piuminirashmika@gmail.com>
2024-03-03 15:56:23 +0530

    Implement BOFactory

--- src/main/java/lk/ijse/BO/BOFactory.java
+++ src/main/java/lk/ijse/BO/BOFactory.java
@@ -1,4 +1,30 @@
 package lk.ijse.BO;
 
 public class BOFactory {
+    private static BOFactory factory;
+
+    private BOFactory() {}
+
+    public static BOFactory getInstance() {
+        return factory == null ? new BOFactory() : factory;
+    }
+
+    public enum BOTypes {
+        USER,CUSTOMER,BOOK,TRANSACTION
+    }
+
+    public SuperBO getBO(BOTypes boTypes) {
+        switch (boTypes) {
+            case USER:
+                return null;
+            case CUSTOMER:
+                return null;
+            case BOOK:
+                return null;
+            case TRANSACTION:
+                return null;
+            default:
+                return null;
+        }
+    }
 }
