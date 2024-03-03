98f5d0adbe619dfefc79dd156319573c75681eb2
PiuminiRash <piuminirashmika@gmail.com>
2024-03-03 15:44:23 +0530

    Implement FactoryConfiguration

--- src/main/java/lk/ijse/Config/FactoryConfiguration.java
+++ src/main/java/lk/ijse/Config/FactoryConfiguration.java
@@ -1,10 +1,42 @@
 package lk.ijse.Config;
 
+import lk.ijse.Entity.Book;
+import lk.ijse.Entity.Customer;
+import lk.ijse.Entity.Transaction;
 import lk.ijse.Entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class FactoryConfiguration {
+import java.io.IOException;
+import java.util.Properties;
 
+public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfig;
+    private final SessionFactory sessionFactory;
+
+    private FactoryConfiguration() {
+        Configuration configuration = new Configuration();
+        Properties properties = new Properties();
+
+        try {
+            properties.load(ClassLoader.getSystemClassLoader().getResourceAsStream("hibernate.Properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        configuration
+                .addAnnotatedClass(Customer.class)
+                .addAnnotatedClass(Book.class)
+                .addAnnotatedClass(Transaction.class)
+                .addAnnotatedClass(User.class);
+        sessionFactory=configuration.setProperties(properties).buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getInstance() {
+        return (null == factoryConfig) ? factoryConfig = new FactoryConfiguration() : factoryConfig;
+    }
+
+    public Session getSession() {
+        return sessionFactory.openSession();
+    }
 }
