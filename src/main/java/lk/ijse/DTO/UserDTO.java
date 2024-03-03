57b57d1e63a65d5082b1b4450b2de21b3d853a2f
PiuminiRash <piuminirashmika@gmail.com>
2024-03-03 22:20:54 +0530

    Update

--- src/main/java/lk/ijse/DTO/UserDTO.java
+++ src/main/java/lk/ijse/DTO/UserDTO.java
@@ -1,4 +1,13 @@
 package lk.ijse.DTO;
 
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class UserDTO {
+    private String mail;
+    private String password;
 }
