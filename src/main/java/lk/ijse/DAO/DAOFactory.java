f95ec6ecb9ba1be801d0047bdbca55236053ba69
PiuminiRash <piuminirashmika@gmail.com>
2024-03-03 16:04:12 +0530

    Implement DAOFactory

--- src/main/java/lk/ijse/DAO/DAOFactory.java
+++ src/main/java/lk/ijse/DAO/DAOFactory.java
@@ -1,4 +1,33 @@
 package lk.ijse.DAO;
 
 public class DAOFactory {
+    private static DAOFactory factory;
+
+    private DAOFactory() {
+    }
+
+    public static DAOFactory getInstance() {
+        return factory == null ? new DAOFactory() : factory;
+    }
+
+    public enum DAOTypes {
+        USER,CUSTOMER,BOOK,TRANSACTION,QUERY
+    }
+
+    public SuperDAO getDAO (DAOTypes daoTypes) {
+        switch (daoTypes) {
+            case USER:
+                return null;
+            case CUSTOMER:
+                return null;
+            case BOOK:
+                return null;
+            case TRANSACTION:
+                return null;
+            case QUERY:
+                return null;
+            default:
+                return null;
+        }
+    }
 }
