9bd7553ee080aaca7916c44fa8f2321dde430289
PiuminiRash <piuminirashmika@gmail.com>
2024-03-03 21:51:46 +0530

    Implement TransactionBO

--- src/main/java/lk/ijse/BO/custom/TransactionBO.java
+++ src/main/java/lk/ijse/BO/custom/TransactionBO.java
@@ -1,4 +1,20 @@
 package lk.ijse.BO.custom;
 
-public interface TransactionBO {
+import lk.ijse.BO.SuperBO;
+import lk.ijse.DTO.BookDTO;
+import lk.ijse.DTO.CustomerDTO;
+import lk.ijse.DTO.TransactionDTO;
+
+import java.util.List;
+
+public interface TransactionBO extends SuperBO {
+    List<TransactionDTO> getAll();
+    List<String> getAllBookId();
+    List<String> getCustomerId();
+    BookDTO getBook(String book);
+    CustomerDTO getCustomer(String customer);
+    boolean saveTransaction(TransactionDTO transactionDTO);
+    boolean updateTransaction(TransactionDTO transactionDTO);
+    boolean deleteTransaction(String transactionId);
+    String getNextId();
 }
