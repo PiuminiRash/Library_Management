91370ec589af3d8d5c395b02aaf4d5ec8e243c3d
PiuminiRash <piuminirashmika@gmail.com>
2024-03-03 15:33:34 +0530

    Create Configuration

--- src/main/java/lk/ijse/Config/FactoryConfiguration.java
+++ src/main/java/lk/ijse/Config/FactoryConfiguration.java
@@ -6,23 +6,5 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class FactoryConfiguration {
-    private static FactoryConfiguration factoryConfiguration;
 
-    private SessionFactory sessionFactory;
-
-    private FactoryConfiguration(){
-        Configuration configuration = new Configuration().configure()
-                .addAnnotatedClass(User.class);
-        sessionFactory = configuration.buildSessionFactory();
-    }
-    public static FactoryConfiguration getInstance(){
-        if (factoryConfiguration==null){
-            factoryConfiguration =  new FactoryConfiguration();
-        }
-        return factoryConfiguration;
-    }
-
-    public Session getSession(){
-        return  sessionFactory.openSession();
-    }
 }
