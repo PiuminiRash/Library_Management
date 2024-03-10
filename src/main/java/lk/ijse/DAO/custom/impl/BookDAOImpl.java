00fb5a2972762d19bde47d834b91f0737cd7f406
PiuminiRash <piuminirashmika@gmail.com>
2024-03-10 23:11:05 +0530

    Updated

--- src/main/java/lk/ijse/DAO/custom/impl/BookDAOImpl.java
+++ src/main/java/lk/ijse/DAO/custom/impl/BookDAOImpl.java
@@ -1,4 +1,42 @@
 package lk.ijse.DAO.custom.impl;
 
-public class BookDAOImpl {
+import lk.ijse.Config.FactoryConfiguration;
+import lk.ijse.DAO.custom.BookDAO;
+import lk.ijse.Entity.Book;
+import org.hibernate.Session;
+
+import java.util.List;
+
+public class BookDAOImpl implements BookDAO {
+    Session session = FactoryConfiguration.getInstance().getSession();
+
+    @Override
+    public boolean save(Book dto) {
+        return false;
+    }
+
+    @Override
+    public boolean update(Book dto) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return false;
+    }
+
+    @Override
+    public List<Book> getAll() {
+        return null;
+    }
+
+    @Override
+    public Book getItem(String id) {
+        return null;
+    }
+
+    @Override
+    public String getNextId() {
+        return null;
+    }
 }
