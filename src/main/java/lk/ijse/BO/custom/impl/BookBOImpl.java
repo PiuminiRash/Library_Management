4619b0839e36c8f28052efb960b7f891e97c18fd
PiuminiRash <piuminirashmika@gmail.com>
2024-03-10 23:11:16 +0530

    Updated

--- src/main/java/lk/ijse/BO/custom/impl/BookBOImpl.java
+++ src/main/java/lk/ijse/BO/custom/impl/BookBOImpl.java
@@ -1,4 +1,38 @@
 package lk.ijse.BO.custom.impl;
 
-public class BookBOImpl {
+import lk.ijse.BO.custom.BookBO;
+import lk.ijse.DTO.BookDTO;
+
+import java.util.List;
+
+public class BookBOImpl implements BookBO {
+    @Override
+    public List<BookDTO> getAll() {
+        return null;
+    }
+
+    @Override
+    public boolean saveBook(BookDTO bookDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean updateBook(BookDTO bookDTO) {
+        return false;
+    }
+
+    @Override
+    public BookDTO getBook(String bookId) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteBook(String bookId) {
+        return false;
+    }
+
+    @Override
+    public String getNextId() {
+        return null;
+    }
 }
