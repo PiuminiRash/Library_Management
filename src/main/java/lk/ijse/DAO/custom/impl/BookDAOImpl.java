236727852f4d8644e9d38740fdfd3cad37351a89
PiuminiRash <piuminirashmika@gmail.com>
2024-03-13 19:29:12 +0530

    implement BookDAOimpl

--- src/main/java/lk/ijse/DAO/custom/impl/BookDAOImpl.java
+++ src/main/java/lk/ijse/DAO/custom/impl/BookDAOImpl.java
@@ -1,10 +1,16 @@
 package lk.ijse.DAO.custom.impl;
 
+import javafx.scene.control.Alert;
 import lk.ijse.Config.FactoryConfiguration;
 import lk.ijse.DAO.custom.BookDAO;
 import lk.ijse.Entity.Book;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.io.Serializable;
 import java.util.List;
 
 public class BookDAOImpl implements BookDAO {
@@ -12,22 +18,65 @@ public class BookDAOImpl implements BookDAO {
 
     @Override
     public boolean save(Book dto) {
-        return false;
+       try {
+           Transaction transaction = session.beginTransaction();
+           Serializable save = (Serializable) session.save(dto);
+           transaction.commit();
+           return save!=null;
+       } catch (Exception e) {
+           e.printStackTrace();
+           return false;
+       } finally {
+           session.close();
+       }
     }
 
     @Override
     public boolean update(Book dto) {
-        return false;
+         try {
+             Transaction transaction = session.beginTransaction();
+             session.update(dto);
+             transaction.commit();
+             return true;
+         } catch (Exception e) {
+             new Alert(Alert.AlertType.ERROR,e.getMessage()).show();
+             return false;
+         } finally {
+             session.close();
+         }
     }
 
     @Override
     public boolean delete(String id) {
-        return false;
+        try {
+            Transaction transaction = session.beginTransaction();
+            Book book = session.get(Book.class,id);
+            session.delete(book);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            return false;
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public List<Book> getAll() {
-        return null;
+        try {
+            Transaction transaction = session.beginTransaction();
+            CriteriaBuilder criteriaBuilder = (CriteriaBuilder) session.getCriteriaBuilder();
+            CriteriaQuery<Book> query = criteriaBuilder.createQuery(Book.class);
+            query.from(Book.class);
+            List<Book> resultList = session.createQuery(String.valueOf(query)).getResultList();
+            transaction.commit();
+            return resultList;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            session.close();
+        }
     }
 
     @Override
@@ -37,6 +86,17 @@ public class BookDAOImpl implements BookDAO {
 
     @Override
     public String getNextId() {
-        return null;
+        try {
+            String newId = "B000";
+            Transaction transaction = session.beginTransaction();
+            List list = session.createNativeQuery("select book_id from book order by book_id desc limit 1").list();
+            if (!list.isEmpty()) newId = (String) list.get(0);
+            transaction.commit();
+            session.close();
+            return newId;
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
