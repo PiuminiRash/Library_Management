d295cd55fe19706bba8a6998d58d49c69f4234be
PiuminiRash <piuminirashmika@gmail.com>
2024-03-15 18:10:11 +0530

    Update

--- src/main/java/lk/ijse/DTO/TransactionDTO.java
+++ src/main/java/lk/ijse/DTO/TransactionDTO.java
@@ -5,17 +5,25 @@ import lombok.*;
 import java.util.Date;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Data
 @Getter
 @Setter
+@ToString
 public class TransactionDTO {
     private String transId;
     private Date startDate;
     private Date endDate;
-    private String cusId;
+    private String userId;
     private String bookId;
     private String status;
-    private String cusName;
-    private String bookName;
+//    private String userName;
+//    private String bookName;
+
+    public TransactionDTO(String transId,Date startDate,Date endDate,String userId,String bookId) {
+        this.transId = transId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.userId = userId;
+        this.bookId = bookId;
+    }
 }
